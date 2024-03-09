e905ad68179e161f51244b5d577a843ac16c87d8
iwaking2024 <149971570+iwaking2024@users.noreply.github.com>
2024-03-08 21:49:19 -0600

    Update _Antispam.js

--- plugins/_Antispam.js
+++ plugins/_Antispam.js
@@ -46,12 +46,12 @@ export async function before(m) {
             setTimeout(() => {
                 users[m.sender].banned = false;
                 this.spam[m.sender].count = 0;
-                m.reply(`✅ *Cooldown finished*\nYou can send messages again.`);
+                m.reply(`✅ *Enfriamiento terminado*\nPuedes enviar mensajes nuevamente.`);
             }, 5000);
 
             // Notify the sender about the spamming and the remaining cooldown time.
             const message = m.mtype.replace(/message$/i, '').replace('audio', m.msg.ptt ? 'PTT' : 'audio').replace(/^./, v => v.toUpperCase()) || 'Unknown';
-            return m.reply(`❌ *Please do not spam ${message}*\nWait for ${Math.ceil((this.spam[m.sender].lastspam - now) / 1000)} seconds`);
+            return m.reply(`❌ *Por favor no envíes spam ${message}*\nWait for ${Math.ceil((this.spam[m.sender].lastspam - now) / 1000)} seconds`);
         }
     } else {
         // If the time difference is greater than or equal to 10 seconds, reset the sender's spam count.
