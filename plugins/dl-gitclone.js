4b351caab7815b005ba179bb102ab4f7347f75ec
iwaking2024 <149971570+iwaking2024@users.noreply.github.com>
2024-03-08 22:21:11 -0600

    Update dl-gitclone.js

--- plugins/dl-gitclone.js
+++ plugins/dl-gitclone.js
@@ -2,14 +2,14 @@
 import fetch from 'node-fetch'
 const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `where is the github link?\n\n📌 Example : ${usedPrefix + command} https://github.com/Guru322/api`
+    if (!args[0]) throw `¿Dónde está el enlace de github?\n\n📌 Ejemplo : ${usedPrefix + command} https://github.com/Guru322/api`
     if (!regex.test(args[0])) throw '⚠️ link incorrect'
     let [_, user, repo] = args[0].match(regex) || []
     repo = repo.replace(/.git$/, '')
     let url = `https://api.github.com/repos/${user}/${repo}/zipball`
     let filename = (await fetch(url, { method: 'HEAD' })).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
   
-    m.reply(`✳️ *Wait, sending repository..*`)
+    m.reply(`✳️ *Espera, enviando repositorio..*`)
     conn.sendFile(m.chat, url, filename, null, m)
 }
 handler.help = ['gitclone <url>']
