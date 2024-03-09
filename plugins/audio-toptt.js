d94844acc5645cb15a6c90a6d015c73180133f0e
iwaking2024 <149971570+iwaking2024@users.noreply.github.com>
2024-03-08 22:13:58 -0600

    Update audio-toptt.js

--- plugins/audio-toptt.js
+++ plugins/audio-toptt.js
@@ -3,11 +3,11 @@ import { toPTT } from '../lib/converter.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
     let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-    if (!/video|audio/.test(mime)) throw `✳️ Reply to the audio you want to convert to voice memo with :\n *${usedPrefix + command}*`
+    if (!/video|audio/.test(mime)) throw `✳️Responde al audio que deseas convertir en nota de voz con :\n *${usedPrefix + command}*`
     let media = await q.download?.()
-    if (!media) throw '❎ Failed to download media'
+    if (!media) throw '❎ No se pudo descargar el medio'
     let audio = await toPTT(media, 'mp4')
-    if (!audio.data) throw '❎ Error converting'
+    if (!audio.data) throw '❎ Error al convertir'
     conn.sendFile(m.chat, audio.data, 'audio.mp3', '', m, true, { mimetype: 'audio/mp4' })
 }
 handler.help = ['toav']
