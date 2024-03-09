8d7958835e44002d3e151177dfd8ddc1f09ac16c
iwaking2024 <149971570+iwaking2024@users.noreply.github.com>
2024-03-08 22:23:54 -0600

    Update dl-instagram.js

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -1,20 +1,20 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, usedPrefix, args, command, text }) => {
-  if (!text) throw `You need to give the URL of Any Instagram video, post, reel, image`;
+  if (!text) throw `Debes proporcionar la URL de cualquier video, publicación, carrete o imagen de Instagram.`;
   m.reply(wait);
 
   let res;
   try {
     res = await fetch(`${gurubot}/igdlv1?url=${text}`);
   } catch (error) {
-    throw `An error occurred: ${error.message}`;
+    throw `Ocurrió un error: ${error.message}`;
   }
 
   let api_response = await res.json();
 
   if (!api_response || !api_response.data) {
-    throw `No video or image found or Invalid response from API.`;
+    throw `No se encontró ningún video o imagen o respuesta no válida de la API.`;
   }
 
   const mediaArray = api_response.data;
@@ -23,7 +23,7 @@ let handler = async (m, { conn, usedPrefix, args, command, text }) => {
     const mediaType = mediaData.type;
     const mediaURL = mediaData.url_download;
 
-    let cap = `HERE IS THE ${mediaType.toUpperCase()} >,<`;
+    let cap = `AQUÍ ESTÁ EL ${mediaType.toUpperCase()} >,<`;
 
     if (mediaType === 'video') {
       
