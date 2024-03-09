03b509fc1e52931a96fa1d678d09031ac19c3944
iwaking2024 <149971570+iwaking2024@users.noreply.github.com>
2024-03-08 23:20:07 -0600

    Update gp-delwarn.js

--- plugins/gp-delwarn.js
+++ plugins/gp-delwarn.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args, groupMetadata}) => {
         let who
         if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
         else who = m.chat
-        if (!who) throw `✳️ Tag or mention someone`
+        if (!who) throw `✳️ Etiqueta o menciona a alguien`
         if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
        let warn = global.db.data.users[who].warn
        if (warn > 0) {
@@ -12,9 +12,9 @@ let handler = async (m, { conn, args, groupMetadata}) => {
          
 ▢ Warns: *-1*
 ▢ Warns total: *${warn - 1}*`)
-         m.reply(`✳️ An admin lowered their warning, now you have *${warn - 1}*`, who)
+         m.reply(`✳️ Un administrador redujo su advertencia, ahora tienes *${warn - 1}*`, who)
          } else if (warn == 0) {
-            m.reply('✳️ The user has no warning')
+            m.reply('✳️ El usuario no tiene aviso')
         }
 
 }
