8e0e35f6d87b2cbb373c74cab51e3e3fb906d085
iwaking2024 <149971570+iwaking2024@users.noreply.github.com>
2024-03-08 22:12:01 -0600

    Update anime-waifupics.js

--- plugins/anime-waifupics.js
+++ plugins/anime-waifupics.js
@@ -9,13 +9,13 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   const fetchImage = async (endpoint) => {
     try {
       const response = await fetch(baseUrl + endpoint);
-      if (!response.ok) throw `❎ Error fetching ${type} image`;
+      if (!response.ok) throw `❎ Error al recuperar ${type} imagen`;
       const imageBuffer = await response.buffer(); // Get the image data as a buffer
       conn.sendFile(m.chat, imageBuffer, 'img.jpg', `✅ Random ${type}`, m);
       m.react(dmoji);
     } catch (error) {
       console.error(error);
-      m.reply(`❎ An error occurred while fetching the ${type} image.`);
+      m.reply(`❎Se produjo un error al recuperar la ${type} imagen.`);
     }
   };
 
