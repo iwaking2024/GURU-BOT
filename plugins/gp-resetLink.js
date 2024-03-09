6fd11ab27ec040d1b3dfbb31203fc6e1778d138a
iwaking2024 <149971570+iwaking2024@users.noreply.github.com>
2024-03-08 23:35:50 -0600

    Update gp-resetLink.js

--- plugins/gp-resetLink.js
+++ plugins/gp-resetLink.js
@@ -1,7 +1,7 @@
 
 let handler = async (m, { conn }) => {
   let res = await conn.groupRevokeInvite(m.chat)
-  m.reply('✅ Group link has been successfully reset\n\n📌 new link:\nhttps://chat.whatsapp.com/' + res)
+  m.reply('✅ El enlace del grupo se ha restablecido correctamente \n\n📌 new link:\nhttps://chat.whatsapp.com/' + res)
 }
 handler.help = ['resetlink']
 handler.tags = ['group']
