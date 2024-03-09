5db50db952fe8709bb714a73fa767037c9769c51
iwaking2024 <149971570+iwaking2024@users.noreply.github.com>
2024-03-08 23:37:49 -0600

    Update gp-setwelcome.js

--- plugins/gp-setwelcome.js
+++ plugins/gp-setwelcome.js
@@ -3,8 +3,8 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
     global.db.data.chats[m.chat].sWelcome = text
-    m.reply('✅ The welcome message is configured')
-  } else throw `✳️ Enter the Welcome message\n\n@user (mention)\n@group (Group name)\n@desc (description of group)`
+    m.reply('✅ El mensaje de bienvenida está configurado.')
+  } else throw `✳️Escriba el mensaje de bienvenida\n\n@user (mention)\n@group (Group name)\n@desc (description of group)`
 }
 handler.help = ['setwelcome <text>']
 handler.tags = ['group']
